7f30a48fe2c9615cd5f76735fae047cb0c1937fc
quang.trieu <quang.trieu@code4you.com>
2026-06-17 11:48:24 +0200

    update release

--- Formula/dbtools.rb
+++ Formula/dbtools.rb
@@ -4,7 +4,7 @@ class Dbtools < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/quang2002/dbtools/releases/download/v1.0.0/dbtools-osx-arm64.tar.gz"
-    sha256 "a2a28ef87d22304b08b5afd8c3b668ac7f6a6f6220846e4029c81ea28ed0f25b"
+    sha256 "213f21805163cee33f2fd4c959e4d35b825d5f375b58d9db913ffbb35c9898a1"
   end
 
   version "1.0.0"
